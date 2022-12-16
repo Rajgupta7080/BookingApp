3b34fceb670ff8f484aa6757782b88b490fc0e78
Rajgupta7080 <rajgupta09875@gmail.com>
2022-12-16 17:29:57 +0530

    HotelCRUD Operations API completed

--- BookingAppBackend/Routes/hotels.js
+++ BookingAppBackend/Routes/hotels.js
@@ -12,9 +12,50 @@ router.post('/', async (req, res) => {
     res.status(500).json(error)
   }
 })
-// update
+// update(Put)
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedhotel = await HotelModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      },
+    )
+    res.status(200).json(updatedhotel)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 // delete
-// put
-// patch
+router.delete('/:id', async (req, res) => {
+  try {
+    await HotelModel.findByIdAndDelete(req.params.id)
+    res.status(200).json('Deleted succesfully')
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+// get
+router.get('/:id', async (req, res) => {
+  try {
+    const hotel = await HotelModel.findById(req.params.id)
+    res.status(200).json(hotel)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// getall
+router.get('/', async (req, res) => {
+  try {
+    const hotelAll = await HotelModel.find()
+    res.status(200).json(hotelAll)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 export default router
